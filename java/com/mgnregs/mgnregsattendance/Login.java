182de038c92fd2d0930330e4c1981c6890d3d902
Vivek Patil <vivekpatil00@gmail.com>
2017-03-12 22:01:20 +0530

    Initial commit

---
+++ java/com/mgnregs/mgnregsattendance/Login.java
@@ -0,0 +1,21 @@
+package com.mgnregs.mgnregsattendance;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+
+/**
+ * Created by Vivek on 12-03-2017.
+ */
+
+public class Login extends Activity{
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.loginscreen);
+    }
+    @Override
+    protected void onStart() {
+        super.onStart();
+    }
+}
---
+++ java/com/mgnregs/mgnregsattendance/Welcome_splash.java
@@ -0,0 +1,39 @@
+package com.mgnregs.mgnregsattendance;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+
+/**
+ * Created by Vivek on 12-03-2017.
+ */
+
+public class Welcome_splash extends Activity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_welcome_splash);
+        Thread timer=new Thread(){
+            public void run(){
+                try{
+                    sleep(2000);
+                }
+                catch (InterruptedException ie)
+                {
+                    ie.printStackTrace();
+                }
+                finally{
+                    Intent login=new Intent("com.mgnregs.mgnregsattendance.Login");
+                    startActivity(login);
+                }
+            }
+        };
+        timer.start();
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+    }
+}
